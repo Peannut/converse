2baa00806e07c103062c5cb975028ae27cf9a778
Zakaria Oukaddour <47929413+Peannut@users.noreply.github.com>
2024-01-30 17:37:32 +0100

    header is ready to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 
+import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
    <main className='relative'>
-      <div>
-        <h1 className='text-3xl text-red-500'>Hello, world!</h1>
-        <p className='text-sky-400'>The quick brown fox...</p>
-
-        
-      </div>
+      <Navbar />
     </main>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// changing the port
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3005
+  }
 })
