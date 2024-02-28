829e7296eeb4eee4012328f39a64596990047db2
Peannut <zakariaspam0@gmail.com>
2024-02-28 02:01:11 +0100

    ready for deployment build is good 😎

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
-import { navLinks } from "../assets/constants/index.ts";
+import { navLinks } from "../assets/constants/index.js";
 
 const Nav = () => {
   return (
